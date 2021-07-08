4fb6ef453d88762291fa2423f3a0b78a600223c3
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-08 12:22:33 +1000

    Completed
    Silly name essentially means find the median number in an array. For even arrays I selected the leftmost one.

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,5 +1,18 @@
+// export function absoluteValuesSumMinimization(a: number[]): number {
+//     let arrayLength = a.length;
+//     if (arrayLength % 2 === 0) {
+//         arrayLength = arrayLength / 2;
+//     } else {
+//         arrayLength = Math.floor(arrayLength / 2) + 1;
+//     }
+//     let num = a[arrayLength];
+//     return num;
+// }
+
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+    const isEven = a.length % 2 === 0;
+
+    return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)]
 }
 
 // console.log(absoluteValuesSumMinimization([2, 4, 7]));
