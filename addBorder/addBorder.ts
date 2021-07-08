d959c53a269bc849922a494d022cbc48e66ec021
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-08 12:50:21 +1000

    Completed Borders
    Also added comments

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,17 @@
-export function addBorder(picture: string[]): string[] {
+export function addBorder(picture: string[]): any {
+  // build a wall
+  const wall = '*'.repeat(picture[0].length + 2);
 
+  // wall the front and back of the array
+  picture.unshift(wall);
+  picture.push(wall);
+
+  // put up a wall on the sides
+  for (let i = 1; i < picture.length - 1; i++) {
+    picture[i] = `*${picture[i]}*`;
+  }
+
+  return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
