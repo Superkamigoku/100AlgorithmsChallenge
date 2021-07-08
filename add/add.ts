b977862137ce4a169b136f105cea4e77d6c38dd6
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-08 11:48:53 +1000

    Completed "add"

--- add/add.ts
+++ add/add.ts
@@ -1,8 +1,13 @@
 export function add(param1: number, param2: number): number {
-
+  return param1 + param2;
 }
 
 export function add2(...param1: number[]): number {
+  let sum = 0;
+
+  for (let param of param1) sum += param;
+
+  return sum;
 
 }
 // console.log(add(1, 2));
@@ -10,3 +15,4 @@ export function add2(...param1: number[]): number {
 
 // console.log(add2(1,2,3,4,5));
 // console.log(add2(2,3));
+//
