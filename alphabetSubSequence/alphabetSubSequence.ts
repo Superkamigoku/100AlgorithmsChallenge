0cabab4d9f1726c670a563cd34a94fa1e3dc39f6
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-13 14:30:45 +1000

    Completed AlphabetSubsequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,26 @@
 export function alphabetSubsequence(s: string): boolean {
+  // break string down
+  const arr: string[] = s.split('');
+  // create a set for its methods
+  const set1 = new Set();
 
+  for (let i = 0; i < arr.length; i++) {
+    set1.add(arr[i]);
+  }
+  // if multiple then return false
+  if (set1.size !== arr.length) {
+    return false;
+  }
+
+  // no multiples
+  for (let i = 0; i < arr.length - 1; i++) {
+    // the letter has to be greater than the letter before it (according to char codes)
+    if (arr[i].charCodeAt() > arr[i + 1].charCodeAt()) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 console.log(alphabetSubsequence('zab'))
