18891a8f9cbd4b7d3e859a707df155058027421b
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-13 12:10:34 +1000

    Completed allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,20 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+  let longestLength = 0;
+  let longestWords = [];
 
+  // finds the longest length of a string
+  inputArray.forEach((word: string) => {
+    longestLength = longestLength > word.length ? longestLength : word.length;
+  })
+
+  // add to longest words
+  inputArray.forEach((word: string) => {
+    if (word.length === longestLength) {
+      longestWords.push(word);
+    }
+  })
+
+  return longestWords;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
