cb2e69190a895185f110a6ab8e2b75fe1675e2d0
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-19 15:24:43 +1000

    Completed characterParity

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,14 @@
 export function characterParity(symbol: string): string {
 
+  if (isNaN(parseInt(symbol))) {
+    return "not a digit"
+  } else if (parseInt(symbol) % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
 }
 
-// console.log(characterParity('5'))
-// console.log(characterParity('8'))
-// console.log(characterParity('q'))
+console.log(characterParity('5'))
+console.log(characterParity('8'))
+console.log(characterParity('q'))
