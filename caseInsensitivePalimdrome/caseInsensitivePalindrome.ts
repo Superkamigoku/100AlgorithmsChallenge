7c08cca6adac4731fa263a6c7ba088a600d72f35
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-19 15:12:08 +1000

    Completed caseInsensitivePalindrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,10 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
+  let original = inputString.toLowerCase();
+  // uses the split to turn to array so can run reverse
+  let reversed = original.split('').reverse().join('');
 
+  return original === reversed;
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('AaBaa'));
+console.log(isCaseInsensitivePalindrome('abac'));
