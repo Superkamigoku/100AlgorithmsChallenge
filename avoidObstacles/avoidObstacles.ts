836d69c269076fb9670aaa01d2b93fb5a148d278
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-19 12:29:41 +1000

    Completed avoidObstacles

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,14 @@
 export function avoidObstacles(inputArray: number[]): number {
+  // quick sort to get the largest number for loop length limit
+  inputArray.sort();
+  let largest = inputArray[inputArray.length - 1];
 
+  // if divisible then return jump
+  for (let jump = 2; jump <= largest; jump++) {
+    if (inputArray.every((element) => element % jump !== 0)) {
+      return jump;
+    }
+  }
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([5, 3, 6, 7, 9]));
