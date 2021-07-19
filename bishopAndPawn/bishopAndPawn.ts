0f26b995815b9fb7b5fdd39458241eb04cebe3fd
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-19 13:49:25 +1000

    Completed bishopAndPawn

--- bishopAndPawn/bishopAndPawn.ts
+++ bishopAndPawn/bishopAndPawn.ts
@@ -1,5 +1,16 @@
 export function bishopAndPawn(bishop: string, pawn: string): boolean {
-
+  const stringConvert: object = {
+    a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8
+  }
+  let bishopArr = bishop.split('');
+  let pawnArr = pawn.split('');
+  // Using math.abs you get a positive number nomatter what ;D
+  let move = Math.abs(bishop[1] - pawn[1]);
+  // if the letter math.abs is the same as the number it is always true (since that makes a diagonal)
+  if (Math.abs(stringConvert[bishop[0]] - stringConvert[pawn[0]]) === move) {
+    return true;
+  }
+  return false;
 }
 
-// console.log(bishopAndPawn('a1', 'c3'));
+console.log(bishopAndPawn('a1', 'c3'));
