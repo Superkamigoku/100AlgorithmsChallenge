11d8974465389f4a8626776eb052e4451c822381
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-19 11:19:59 +1000

    Completed arrayMaximalAdjacentDifference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,16 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+  let sum = 0;
+  let max = 0
 
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    sum = Math.abs(inputArray[i] - inputArray[i + 1]);
+
+    if (sum > max) {
+      max = sum;
+    }
+  }
+
+  return max;
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
