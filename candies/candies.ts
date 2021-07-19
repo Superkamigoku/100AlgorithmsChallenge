cb6f56bf3123ade26311f3a309b5dd8ab8e10701
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-19 14:51:57 +1000

    Completed candies

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,7 @@
-export function candies(n: number, m: number): number {
-
+// i changed the variables since it isnt clear which number relates to what thing
+export function candies(children: number, candy: number): number {
+  // returns the amount of candy that is eaten if it is evenly split
+  return Math.floor(candy / children) * children;
 }
 
-// console.log(candies(3, 10));
+console.log(candies(3, 10));
