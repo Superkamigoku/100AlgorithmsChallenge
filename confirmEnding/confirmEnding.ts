e4247bc98bcb3c458c0c47236602935b6b82213e
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-20 13:59:41 +1000

    Completed comfirmEnding

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,7 @@
 export function confirmEnding(str: string, target: string) {
-
+  const sub = str.substring(target.length - 1);
+  return sub === target;
 }
 
-// console.log(confirmEnding("Abstraction", "action"));
-// console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding("Abstraction", "action"));
+console.log(confirmEnding("Open sesame", "pen"));
