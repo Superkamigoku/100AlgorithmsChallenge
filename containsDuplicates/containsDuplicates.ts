033c98141636ca7381067c9ea6a5657920548810
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-20 14:48:24 +1000

    Completed containsDuplicates

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,6 +1,15 @@
 export function containsDuplicates(a: number[]): boolean {
-
+  // make object that stores if that value has appeared
+  let duplicateObj = {};
+  for (let i = 0; i < a.length; i++) {
+    if (duplicateObj[a[i]]) {
+      return true
+    } else {
+      duplicateObj[a[i]] = 1;
+    }
+  }
+  return false;
 }
 
-// console.log(containsDuplicates([1, 2, 3, 1]));
-// console.log(containsDuplicates([3, 1]));
+console.log(containsDuplicates([1, 2, 3, 1]));
+console.log(containsDuplicates([3, 1]));
