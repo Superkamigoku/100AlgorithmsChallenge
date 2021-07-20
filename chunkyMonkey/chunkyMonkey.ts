55668b0c9d3fd367053b805e7ed586881565cc2d
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-20 10:09:48 +1000

    Completed chunkyMonkey

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,12 @@
-export function chunkyMonkey(arr: any[], size: number): any[][] {
-
+export function chunkyMonkey(arr, size) {
+  let finalArr = [];
+  let count = 0;
+  while (count < arr.length) {
+    // push, slice, and modifying count on the same line :D
+    finalArr.push(arr.slice(count, count += size));
+  }
+  return finalArr;
 }
 
-// console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
-// console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
