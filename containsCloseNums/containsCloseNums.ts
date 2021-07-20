be2b713074619047edbbd268056e44fdf4f74d3e
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-20 14:48:13 +1000

    Completed containsCloseNums

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,9 +1,9 @@
 export function containsCloseNums(nums: number[], k: number): boolean {
-  let current = nums[0];
-  for (let i = 0; i < nums.length - 1; i++) {
-    // if Math.abs ever returns k then it is true
-    if (Math.abs(nums[i] - nums[i + 1]) === k) {
-      return true;
+  for (let i = 0; i < nums.length - k; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (j !== i && nums[i] === nums[j] && Math.abs(i - j) <= k) {
+        return true;
+      }
     }
   }
   return false;
@@ -11,4 +11,4 @@ export function containsCloseNums(nums: number[], k: number): boolean {
 
 console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
 console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
-console.log(containsCloseNums([0, 1, 2, 3, 5, 1], 3));
+console.log(containsCloseNums([0, 1, 2, 3, 5, 1], 1));
