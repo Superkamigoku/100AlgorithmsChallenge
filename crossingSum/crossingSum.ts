c99cf7b1e269761551ba944fa8061b2d25e51111
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-21 11:46:27 +1000

    Completed crossingSum

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,17 @@
 export function crossingSum(matrix: number[][], a: number, b: number): number {
+  // counts the whole row
+  let rowTotal = matrix[a].reduce((a, b) => a + b);
+  let columnTotal = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    // goes through the rows and adds only the one on column b if it isnt on row a
+    columnTotal += i !== a ? matrix[i][b] : 0;
+  }
 
+  return rowTotal + columnTotal;
 }
 
-// console.log(crossingSum([[1, 1, 1, 1], 
-//     [2, 2, 2, 2], 
-//     [3, 3, 3, 3]], 1, 3));
+
+
+console.log(crossingSum([[1, 1, 1, 1],
+[2, 2, 2, 2],
+[3, 3, 3, 3]], 1, 3));
