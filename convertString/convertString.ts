cb8799e30356b33f4b76bce49c906429ce4bc8af
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-21 11:16:49 +1000

    Completed convertString

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,6 +1,17 @@
 export function convertString(s: string, t: string): boolean {
+  let word = '';
 
+  // init j here since it doesnt need to be a loop
+  let j = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === t[j]) {
+      // loop iterates through first string to find letters of second string
+      word = word.concat(s[i]);
+      j++;
+    }
+  }
+  return word === t;
 }
 
-// console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
-// console.log(convertString('addbyca', 'abcd'));
+console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
+console.log(convertString('addbyca', 'abcd'));
