90afbad24255bac27a6071a4834d939b2fc4b4b3
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-21 13:51:38 +1000

    Completed depositProfit

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,12 @@
 export function depositProfit(deposit: number, rate: number, threshold: number): number {
 
+  let years = 0;
+  let currentDeposit = deposit;
+  while (currentDeposit < threshold) {
+    currentDeposit *= ((rate / 100) + 1);
+    years++;
+  }
+  return years;
 }
 
-// console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170))
