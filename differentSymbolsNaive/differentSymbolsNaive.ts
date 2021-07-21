6105e90d6216eb4f9f2595f817a4ba752aaefb12
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-21 13:56:21 +1000

    Completed differentSymbolsNaive

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,16 @@
 export function differentSymbolsNaive(s: string): number {
+  //Could use sets or an object instead of this solution (which would work better)
+  //but wanted to try something different
+  const uniqueChars: string[] = [];
+  const inputChars: string[] = s.split('');
 
+  inputChars.forEach((char) => {
+    if (!uniqueChars.includes(char)) {
+      uniqueChars.push(char);
+    }
+  });
+
+  return uniqueChars.length;
 }
 
-// console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive('cabca'));
