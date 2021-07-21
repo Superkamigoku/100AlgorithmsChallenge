682db844e7c1c253e9d1052b91c817f2e02b6fef
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-22 09:57:07 +1000

    Completed digitDegree

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,20 @@
 export function digitDegree(n: number): number {
+  let count = 0;
+  let digits = n;
+  while (true) {
+    if (digits.toString().length === 1) {
+      return count;
+    } else {
+      count++;
+    }
 
+    // modify digits to reduce, which requires an array, which requires a string
+    digits = digits.toString().split('').reduce((acc, num) => {
+      return acc + parseInt(num);
+    }, 0);
+  }
 }
 
-// console.log(digitDegree(5));
-// console.log(digitDegree(10));
-// console.log(digitDegree(91));
+console.log(digitDegree(5));
+console.log(digitDegree(10));
+console.log(digitDegree(91));
