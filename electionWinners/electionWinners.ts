0e69257bc9c7909949b43fd82207b4df4c3b43c4
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-22 10:25:31 +1000

    Completed electionWinners

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,13 @@
 export function electionsWinners(votes: number[], k: number): number {
-
+  const sortedVotes = votes.sort();
+  const mostVotes = sortedVotes[sortedVotes.length - 1];
+  let inTheRunning = 0;
+  for (let i = 0; i < sortedVotes.length; i++) {
+    if (sortedVotes[i] + k > mostVotes) {
+      inTheRunning++
+    }
+  }
+  return inTheRunning;
 }
 
-// console.log(electionsWinners([2, 3, 5, 2], 3));
+console.log(electionsWinners([2, 3, 5, 2], 3));
