da08c1cf4af2df9888ab5acda29862820381b778
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-22 11:25:23 +1000

    Completed evenDigitsOnly

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,6 +1,27 @@
-export function evenDigitsOnly(n: number): boolean {
+export function evenDigitsOnly(n): boolean {
+  // This would be much easier if i used arrays but i wanted to try it without
+  // requires a lot of variables because they need to be modified
+  // numberLength starts at 0 so i can change it at the start of the loop
+  let number = n;
+  let numberParity = 2;
+  let numberStr = '';
+  let numberLength = 0;
+  // if parity is higher than the number then it passed all the tests
+  while (numberParity < number) {
+    if (number % numberParity === 0) {
+      numberParity *= 10;
+      numberLength++;
+      numberStr = number.toString();
+      // each loop i take off the last digit but it doesnt remove the 0 that is left over
+      number -= parseInt(numberStr[numberStr.length - numberLength]) * (Math.pow(10, numberLength) / 10)
+    } else {
+      // fail parity test
+      return false;
+    }
+  }
 
+  return true;
 }
 
-// console.log(evenDigitsOnly(248622));
-// console.log(evenDigitsOnly(642386));
+console.log(evenDigitsOnly(248622));
+console.log(evenDigitsOnly(642386));
