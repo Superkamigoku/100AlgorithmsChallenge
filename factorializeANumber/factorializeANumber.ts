172ab034a32dc29babfa7e1624dc6fd0761ac4d4
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-22 12:02:05 +1000

    Completed factorializeNumber

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,12 @@
 export function factorializeANumber(num: number): number {
+  let factorial = 1;
 
+  for (let i = 2; i <= num; i++) {
+    factorial *= i;
+  };
+
+  return factorial;
 }
 
-// console.log(factorializeANumber(5));
-// console.log(factorializeANumber(10));
+console.log(factorializeANumber(5));
+console.log(factorializeANumber(10));
