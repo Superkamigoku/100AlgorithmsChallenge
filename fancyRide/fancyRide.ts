fa22e559902530a9e408838d2eae9749e40b91ef
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-22 12:12:56 +1000

    Completed fancyRide

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,15 @@
 export function fancyRide(l: number, fares: number[]): string {
+  // this array is from the question
+  let rides: string[] = ['UberX', 'UberXL', 'UberPlus', 'UberBlack', 'UberSUV'];
+  let bestRide = '';
 
+  fares.forEach((ride, index) => {
+    if (ride * l <= 20) {
+      bestRide = rides[index];
+    }
+  });
+
+  return bestRide;
 }
 
-// console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
+console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
