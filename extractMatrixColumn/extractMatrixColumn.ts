4920cb5203ee23a8c2c1663d9a0390e73972e86d
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-22 11:53:50 +1000

    Completed extractMatrixColumn

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,11 @@
 export function extractMatrixColumn(matrix: number[][], column: number): number[] {
+  let columnArr: number[] = [];
 
+  for (let i = 0; i < matrix.length; i++) {
+    columnArr.push(matrix[i][column])
+  }
+
+  return columnArr;
 }
 
-// console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
+console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
