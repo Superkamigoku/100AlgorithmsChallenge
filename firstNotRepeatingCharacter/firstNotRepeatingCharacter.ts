5322a65617f0a8b71a33cf6b3028628270cb2162
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-29 09:45:09 +1000

    Completed firstNotRepeatingCharacters

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,14 @@
 export function firstNotRepeatingCharacter(s: string): string {
-
+  for (let i = 0; i < s.length; i++) {
+    // looks at the index of the first instance of the char in the string
+    // if same as last index then it is unique
+    // since going forward thru loop it is the first unique
+    if (s.indexOf(s[i]) === i && s.lastIndexOf(s[i]) === i) {
+      return s[i];
+    }
+  }
+  return '_';
 }
 
-// console.log(firstNotRepeatingCharacter('abacabad'));
-// console.log(firstNotRepeatingCharacter('abacabaabacaba'));
+console.log(firstNotRepeatingCharacter('abacabad'));
+console.log(firstNotRepeatingCharacter('abacabaabacaba'));
