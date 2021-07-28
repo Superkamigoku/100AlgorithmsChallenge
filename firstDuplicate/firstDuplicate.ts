d4fffd394c75b8f42342c3b3736eed7f4554ef0e
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-28 15:37:45 +1000

    Completed firstDuplicate

--- firstDuplicate/firstDuplicate.ts
+++ firstDuplicate/firstDuplicate.ts
@@ -1,6 +1,19 @@
 export function firstDuplicate(a: number[]): number {
+  let duplicates = {};
 
+  for (let i = 0; i < a.length; i++) {
+    let current = parseInt(a[i]);
+    // if already on object then return the number
+    if (duplicates[current] === 1) {
+      return current;
+    }
+    // if not on object then it is unique (so far)
+    duplicates[current] = 1;
+  }
+
+  // if none found then return -1
+  return -1;
 }
 
-// console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
-// console.log(firstDuplicate([2, 4, 3, 5, 1]));
+console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
+console.log(firstDuplicate([2, 4, 3, 5, 1]));
