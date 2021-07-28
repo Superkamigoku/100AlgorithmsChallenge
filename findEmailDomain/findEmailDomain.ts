9a519d0b44d5b46d3555df117c07c4e7874b572a
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-28 14:55:24 +1000

    Completed findEmailDomain

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,6 +1,7 @@
 export function findEmailDomain(address: string): string {
-
+  const domain = address.split('@');
+  return domain[domain.length - 1];
 }
 
-// console.log(findEmailDomain('prettyandsimple@example.com'));
-// console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+console.log(findEmailDomain('prettyandsimple@example.com'));
+console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
