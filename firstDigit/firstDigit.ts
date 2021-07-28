b4f0e9795159f826e887aba8e4af8e00afd303a0
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-28 15:06:59 +1000

    Completed firstDigit

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,7 +1,14 @@
 export function firstDigit(inputString: string): string {
+  const firstDigitArr = inputString.split('');
+
+  for (let i = 0; i < firstDigitArr.length; i++) {
+    if (!isNaN(firstDigitArr[i])) {
+      return firstDigitArr[i].toString();
+    }
+  }
 
 }
 
-// console.log(firstDigit('var_1__Int'));
-// console.log(firstDigit('q2q-q'));
-// console.log(firstDigit('0ss'));
+console.log(firstDigit('var_1__Int'));
+console.log(firstDigit('q2q-q'));
+console.log(firstDigit('0ss'));
