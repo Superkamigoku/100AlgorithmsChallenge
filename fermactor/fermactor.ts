3367678759e27dd0819609d45cf5d64814f03068
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-28 10:03:28 +1000

    Completed fermactor

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,12 @@
+// input is always semiprime number
 export function fermactor(n: number): number[] {
-
+  for (let i = 1; i < n; i++) {
+    // limit j to i since it is always going to be less
+    for (let j = 1; j < i; j++) {
+      if (n === Math.pow(i, 2) - Math.pow(j, 2))
+        return [i, j]
+    }
+  }
 }
 
-// console.log(fermactor(15));
+console.log(fermactor(15));
