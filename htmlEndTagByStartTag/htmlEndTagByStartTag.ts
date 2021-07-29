0adfdf43b6b4c2f7d3fbc681c4b47529d746b92d
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-29 11:48:34 +1000

    Completed htmlEndTagByStartTag

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,15 @@
 export function htmlEndTagByStartTag(startTag: string): string {
+  // requires 2 splits because it might have extra variables after the tag
+  const splitArr = startTag.split(' ');
+  let htmlTag = splitArr[0].split('');
 
+  // adds the extra parts of end tag
+  htmlTag.splice(1, 0, '/');
+  if (htmlTag[htmlTag.length - 1] !== '>') {
+    htmlTag.push('>');
+  }
+  return htmlTag.join('');
 }
 
-// console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-// console.log(htmlEndTagByStartTag('<i>'));
+console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
+console.log(htmlEndTagByStartTag('<i>'));
