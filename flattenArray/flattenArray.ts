9ce7528262fce27561bfc4fd6ed11f26f1842b12
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-29 10:03:22 +1000

    Completed flattenArray

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,6 +1,21 @@
 export function flattenArray(arr: any[]): any[] {
+  const singleArr = [];
 
+  flatten(arr);
+
+  // recursive function, calls itself until the element isnt an array, then pushes it
+  function flatten(array: any[]) {
+    array.forEach((element) => {
+      if (Array.isArray(element)) {
+        flatten(element);
+      } else {
+        singleArr.push(element);
+      }
+    })
+  }
+
+  return singleArr;
 }
 
-// console.log(flattenArray([[["a"]], [["b"]]]));
-// console.log(flattenArray([1, [2], [3, [[4]]]]));
+console.log(flattenArray([[["a"]], [["b"]]]));
+console.log(flattenArray([1, [2], [3, [[4]]]]));
