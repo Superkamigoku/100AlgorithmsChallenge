0e5f6b0a2ba561575307aedf884282a0b03a731a
Superkamigoku <Superkamigoku@hotmail.com>
2021-07-29 11:15:01 +1000

    Completed houseOfCats

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,6 +1,8 @@
 export function houseOfCats(legs: number): number[] {
-
+  return Math.floor(legs / 4) !== 0 ?
+    [Math.floor(legs / 4), Math.floor(legs / 2)] :
+    [Math.floor(legs / 2)];
 }
 
-// console.log(houseOfCats(6));
-// console.log(houseOfCats(2));
+console.log(houseOfCats(6));
+console.log(houseOfCats(2));
