38cd831fd46cc2cad319cdee2f49e5ef388bd645
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-05 11:06:24 +1000

    Completed toFixedWidth

--- integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
@@ -1,7 +1,20 @@
 export function integerToStringOfFixedWidth(number: number, width: number): string {
+  if (width <= 0) {
+    return '';
+  }
 
+  let stringifiedNum = number.toString();
+
+  // One of the methods below adds to string and one takes from it
+  if (width > stringifiedNum.length) {
+    stringifiedNum = stringifiedNum.padStart(width, '0')
+  } else {
+    stringifiedNum = stringifiedNum.slice(-width);
+  }
+
+  return stringifiedNum;
 }
 
-// console.log(integerToStringOfFixedWidth(1234, 2));
-// console.log(integerToStringOfFixedWidth(1234, 4));
-// console.log(integerToStringOfFixedWidth(1234, 5));
+console.log(integerToStringOfFixedWidth(1234, 2));
+console.log(integerToStringOfFixedWidth(1234, 4));
+console.log(integerToStringOfFixedWidth(1234, 6));
