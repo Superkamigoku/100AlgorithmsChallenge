04dfe3d9c04841a062cda9249a29bd72e8347c8c
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-06 11:49:23 +1000

    Completed

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,14 @@
 export function longestDigitsPrefix(inputString: string): string {
+  let digitsPrefix = '';
 
+  for (let char of inputString) {
+    const num = parseInt(char);
+    if (isNaN(num)) {
+      return digitsPrefix;
+    }
+    digitsPrefix += char
+  }
+  return digitsPrefix;
 }
 
-// console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix('123aa1'));
