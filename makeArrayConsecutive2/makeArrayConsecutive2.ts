dcfff5e91a57551cec1111483eceba825e512df8
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-06 12:33:51 +1000

    Completed

--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,13 @@
 export function makeArrayConsecutive2(statues: number[]): number {
+  const sortedStatues = statues.sort((a, b) => a - b);
+  let neededStatues = 0;
 
+  // adds a neededStatues for the difference between the current and the next, minus one for the accepted increment
+  for (let i = 0; i < sortedStatues.length - 1; i++) {
+    neededStatues += sortedStatues[i + 1] - sortedStatues[i] - 1;
+  }
+
+  return neededStatues;
 }
-
-// console.log(makeArrayConsecutive2([6, 2, 3, 8]));
+0
+console.log(makeArrayConsecutive2([6, 2, 3, 8]));
