949ddfc1c3bcbffada026e6a2298b304fc6b4783
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-10 14:11:48 +1000

    Completed

--- missingLetters/missingLetters.ts
+++ missingLetters/missingLetters.ts
@@ -1,8 +1,25 @@
 export function missingLetters(str: string): string {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  let missingLetterStr = '';
+  // defines the letter range
+  let start = alphabet.indexOf(str[0]);
+  const end = alphabet.indexOf(str[str.length - 1]);
 
+
+  console.log(start, end);
+  for (let i = start; i <= end; i++) {
+    if (str.indexOf(alphabet[i]) === -1) {
+      missingLetterStr += alphabet[i];
+    }
+  }
+
+  if (missingLetterStr === '') {
+    return undefined;
+  }
+  return missingLetterStr;
 }
 
-// console.log(missingLetters("bce"));
-// console.log(missingLetters("abce"));
-// console.log(missingLetters("abcdefghjklmno"));
-// console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"));
+console.log(missingLetters("bce"));
+console.log(missingLetters("abce"));
+console.log(missingLetters("abcdejfghklmno"));
+console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"));
