51ccaf43bcb7a9985eb48e505a7fbe4b16140207
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-10 12:27:23 +1000

    Completed

--- minimalNumberOfCoins/minimalNumberOfCoins.ts
+++ minimalNumberOfCoins/minimalNumberOfCoins.ts
@@ -1,5 +1,29 @@
 export function minimalNumberOfCoins(coins: number[], price: number): number {
+  let minCoins = 0;
+  let deductedPrice = price;
+  coins.sort((a, b) => b - a);
 
+  for (let i = 0; i < coins.length; i++) {
+    // if you dont check for 0 and it exists then it will return infinity, which isnt very nice ;D
+    if (coins[i] > 0) {
+      const coinsNeeded = Math.floor(deductedPrice / coins[i])
+      if (coinsNeeded > 0) {
+        minCoins += coinsNeeded;
+        deductedPrice = deductedPrice % coins[i];
+      }
+      if (deductedPrice === 0) {
+        return minCoins;
+      }
+    }
+  }
+
+  // if it gets through the loop without returning it will require 1 more coin
+  if (coins[coins.length - 1] !== 0) {
+    return minCoins += 1;
+  } else {
+    return 0;
+  }
 }
 
-// console.log(minimalNumberOfCoins([1, 2, 10], 28));
+console.log(minimalNumberOfCoins([1, 2, 10], 28));
+console.log(minimalNumberOfCoins([0], 28));
