c7fcb24182cc9a60191cb44bf4eea731d6012c23
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-17 13:00:08 +1000

    Completed

--- pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -1,5 +1,22 @@
 export function pagesNumberingWithInk(current: number, numberOfDigits: number): number {
+  let currentPage = current;
+  let inkLeft = numberOfDigits;
+  let pagesPrinted = 0;
+  let partialPrint = currentPage;
+  while (inkLeft > 0) {
+    if ((partialPrint / 10) < 1) {
+      pagesPrinted++;
+      currentPage++;
+      partialPrint = currentPage;
+    } else {
+      partialPrint /= 10;
+    }
+    inkLeft--;
+  }
 
+  return pagesPrinted;
 }
 
-// console.log(pagesNumberingWithInk('G'));
+console.log(pagesNumberingWithInk(1, 5));
+console.log(pagesNumberingWithInk(21, 5));
+console.log(pagesNumberingWithInk(8, 4));
