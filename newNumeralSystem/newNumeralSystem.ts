99747267695bb5af216e10843919383a9241c4b7
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-17 12:40:30 +1000

    Completed

--- newNumeralSystem/newNumeralSystem.ts
+++ newNumeralSystem/newNumeralSystem.ts
@@ -1,5 +1,21 @@
 export function newNumeralSystem(number: string): string[] {
+  if (number === 'A') {
+    // else A will return ['A+A']
+    return ['A'];
+  }
 
+  let numerals: string[] = [];
+  let startCharCount = 65;
+  let endCharCount = number.charCodeAt(0);
+
+  while (startCharCount <= endCharCount) {
+    const numeral = `${String.fromCharCode(startCharCount)} + ${String.fromCharCode(endCharCount)}`
+    numerals.push(numeral);
+    startCharCount++;
+    endCharCount--;
+  }
+
+  return numerals;
 }
 
-// console.log(newNumeralSystem('G'));
+console.log(newNumeralSystem('G'));
