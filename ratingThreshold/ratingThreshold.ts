9b90eb9f923771412971427e3a8a3a3ccdab7773
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-19 14:28:21 +1000

    Completed

--- ratingThreshold/ratingThreshold.ts
+++ ratingThreshold/ratingThreshold.ts
@@ -1,8 +1,22 @@
 export function ratingThreshold(threshold: number, ratings: number[][]): number[] {
+  let toBeReviewed = [];
 
+  // goes through each pro and if under threshold, it pushes
+  for (let i = 0; i < ratings.length; i++) {
+    let totalRating = 0;
+    for (let j = 0; j < ratings[i].length; j++) {
+      totalRating += ratings[i][j];
+    }
+    if (totalRating / ratings[i].length < threshold) {
+      toBeReviewed.push(i);
+    }
+  }
+
+  return toBeReviewed;
 }
 
-// console.log(ratingThreshold(3.5, 
-//     [[3, 4],
-//     [3, 3, 3, 4],
-//     [4]]));
+console.log(ratingThreshold(3.5,
+  [[3, 4],
+  [3, 3, 3, 4],
+  [4],
+  [4, 2, 1]]));
