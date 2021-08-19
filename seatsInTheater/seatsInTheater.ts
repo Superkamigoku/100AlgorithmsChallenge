62ede1d1641eb33c0a96f4d0e40c9a581bee95f7
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-19 15:48:29 +1000

    Completed

--- seatsInTheater/seatsInTheater.ts
+++ seatsInTheater/seatsInTheater.ts
@@ -1,5 +1,7 @@
 export function seatsInTheater(nCols: number, nRows: number, col: number, row: number): number {
+  // affects everyone behind, not including own row
+  return (nCols - col + 1) * (nRows - row);
 
 }
 
-// console.log(seatsInTheater(16, 11, 5, 3));
+console.log(seatsInTheater(16, 11, 5, 3));
