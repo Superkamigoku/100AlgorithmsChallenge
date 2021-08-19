fab88f5a2c09b12bd913ee6cdf9bdcaaf7e0ad34
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-19 15:48:26 +1000

    Completed

--- reverseAString/reverseAString.ts
+++ reverseAString/reverseAString.ts
@@ -1,6 +1,12 @@
 export function reverseAString(str: string): string {
+  let reverseStr = '';
 
+  // starts at 1 since length -1 is the last char
+  for (let i = 1; i < str.length + 1; i++) {
+    reverseStr += str[str.length - i];
+  }
+  return reverseStr;
 }
 
-// console.log(reverseAString('hello'));
-// console.log(reverseAString('Howdy'));
+console.log(reverseAString('hello'));
+console.log(reverseAString('Howdy'));
