4cf32b25ebb1d3f3f8084bca004bf85d8bdfdc2d
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-19 15:48:37 +1000

    Completed

--- sortByHeight/sortByHeight.ts
+++ sortByHeight/sortByHeight.ts
@@ -1,5 +1,15 @@
 export function sortByHeight(a: number[]): number[] {
+  let filterHeights = a.filter((height) => height !== -1).sort((a, b) => a - b);
 
+  let j = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== -1) {
+      a[i] = filterHeights[j];
+      j++;
+    }
+  }
+
+  return a;
 }
 
-// console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
+console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
