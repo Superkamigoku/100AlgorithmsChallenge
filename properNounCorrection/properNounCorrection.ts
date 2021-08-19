3e9cbc47c80cfd974e1efd5e1ed9f85adb52bdae
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-19 14:28:40 +1000

    Completed

--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,6 +1,7 @@
 export function properNounCorrection(noun: string): string {
-
+  let firstLetter = noun[0].toUpperCase();
+  return firstLetter.concat(noun.toLowerCase().slice(1, noun.length));
 }
 
-// console.log(properNounCorrection('pARiS'));
-// console.log(properNounCorrection('John'));
+console.log(properNounCorrection('pARiS'));
+console.log(properNounCorrection('John'));
