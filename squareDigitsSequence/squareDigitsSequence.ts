521186ba480fc156f7fefe638d5cbc854cb8fd7c
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-19 16:19:16 +1000

    Completed

--- squareDigitsSequence/squareDigitsSequence.ts
+++ squareDigitsSequence/squareDigitsSequence.ts
@@ -1,7 +1,28 @@
 export function squareDigitsSequence(a0: number): number {
+  let digitToSquare = a0.toString();
+  let uniqueNums = {};
+  uniqueNums[digitToSquare] = 1;
+  let count = 1;
 
+  while (true) {
+    count++
+    let totalSquared = 0;
+    for (let i = 0; i < digitToSquare.length; i++) {
+      // squares each number in the digit and adds to total
+      totalSquared += parseInt(digitToSquare[i]) * parseInt(digitToSquare[i]);
+    }
+
+    if (uniqueNums.hasOwnProperty(totalSquared)) {
+      break;
+    }
+    // add to object after the break then reset the digit
+    uniqueNums[totalSquared] = 1;
+    digitToSquare = totalSquared.toString();
+  }
+
+  return count;
 }
 
 
-// console.log(squareDigitsSequence(16));
-// console.log(squareDigitsSequence(103));
+console.log(squareDigitsSequence(16));
+console.log(squareDigitsSequence(103));
