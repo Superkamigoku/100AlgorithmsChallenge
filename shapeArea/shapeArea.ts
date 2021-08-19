0d3b21192ef1fa95ab54c47563113d31381577ef
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-19 15:48:34 +1000

    Completed

--- shapeArea/shapeArea.ts
+++ shapeArea/shapeArea.ts
@@ -1,6 +1,12 @@
 export function shapeArea(n: number): number {
-
+  let area = 1;
+  while (n > 1) {
+    area += (n - 1) * 4;
+    n--;
+  }
+  return area;
 }
 
-// console.log(shapeArea(2));
-// console.log(shapeArea(3));
+console.log(shapeArea(2));
+console.log(shapeArea(3));
+console.log(shapeArea(4));
