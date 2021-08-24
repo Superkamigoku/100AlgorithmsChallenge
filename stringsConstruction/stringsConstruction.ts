aaea7ed07c2a77c24229045c7998eb0473af80d9
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-24 11:42:45 +1000

    Completed

--- stringsConstruction/stringsConstruction.ts
+++ stringsConstruction/stringsConstruction.ts
@@ -1,5 +1,34 @@
 export function stringsConstruction(a: string, b: string): number {
+  let count = Math.floor(b.length / a.length);
+  let countA = getAlphabetCount(a);
+  let countB = getAlphabetCount(b);
 
+  for (let letter in countA) {
+    if (countB.hasOwnProperty(letter)) {
+      if (count > countB[letter]) {
+        count = countB[letter]
+      }
+    } else {
+      // if property doesnt exist then return 0;
+      return 0;
+    }
+  }
+
+  return count;
 }
 
-// console.log(stringsConstruction('abc', 'abccba'));
+function getAlphabetCount(word) {
+  const chars = word.split('');
+  const alphabetCount = {};
+
+  for (let char in chars) {
+    if (alphabetCount.hasOwnProperty(chars[char])) {
+      alphabetCount[chars[char]] += 1;
+    } else {
+      alphabetCount[chars[char]] = 1;
+    }
+  }
+  return alphabetCount;
+}
+
+console.log(stringsConstruction('abc', 'abaccb'));
