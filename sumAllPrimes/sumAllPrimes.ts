a2a7b412b616001431c1beb3c795079624bac02c
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-31 09:40:34 +1000

    Completed

--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,6 +1,29 @@
 export function sumAllPrimes(num: number): number {
+  let primesArray = [];
 
+  for (let currentNum = 1; currentNum <= num; currentNum++) {
+    let isPrime = true;
+    let twoDivisors = 0;
+    console.log(currentNum)
+    for (let upToCurrentNum = 1; upToCurrentNum <= currentNum; upToCurrentNum++) {
+      if (currentNum % upToCurrentNum === 0) {
+        twoDivisors += 1;
+      }
+      if (twoDivisors > 2) {
+        isPrime = false;
+        // break;
+      }
+    }
+
+    if (isPrime) {
+      primesArray.push(currentNum);
+    }
+
+  }
+
+  return primesArray.reduce((a, b) => a + b);
 }
 
-// console.log(sumAllPrimes(10));
-// console.log(sumAllPrimes(977));
+console.log(sumAllPrimes(10));
+console.log(sumAllPrimes(4));
+console.log(sumAllPrimes(977));
