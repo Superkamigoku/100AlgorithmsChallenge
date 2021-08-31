581f900f8f3409afacda4067d41c2b5c21415afa
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-31 10:42:16 +1000

    Completed

--- sumOddFibonacciNums/sumOddFibonacciNums.ts
+++ sumOddFibonacciNums/sumOddFibonacciNums.ts
@@ -1,7 +1,21 @@
 export function sumOddFibonacciNums(num: number): number {
+  let previous = 0;
+  let current = 1;
+  let total = 0;
 
+  while (current <= num) {
+    if (current % 2 !== 0) {
+      total += current;
+    }
+
+    const nextCurrent = previous + current;
+    previous = current;
+    current = nextCurrent;
+  }
+
+  return total;
 }
 
-// console.log(sumOddFibonacciNums(1000));
-// console.log(sumOddFibonacciNums(4000000));
-// console.log(sumOddFibonacciNums(10));
+console.log(sumOddFibonacciNums(1000));
+console.log(sumOddFibonacciNums(4000000));
+console.log(sumOddFibonacciNums(10));
