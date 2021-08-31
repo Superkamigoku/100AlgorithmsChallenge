1fe3f1db75dfa8f6de389f822e01ebcbed075f53
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-31 11:04:44 +1000

    Completed

--- tasksType/tasksType.ts
+++ tasksType/tasksType.ts
@@ -1,6 +1,18 @@
 export function tasksTypes(deadlines: number[], day: number): number[] {
+  let [today, upcoming, later] = [0, 0, 0];
 
+  for (let i = 0; i < deadlines.length; i++) {
+    if (deadlines[i] <= day) {
+      today++;
+    } else if (deadlines[i] <= day + 7) {
+      upcoming++;
+    } else {
+      later++;
+    }
+  }
+
+  return [today, upcoming, later];
 }
 
-// console.log(tasksTypes([1, 2, 3, 4, 5], 2));
-// console.log(tasksTypes([1, 2, 4, 2, 10, 3, 1, 4, 5, 4, 9, 8], 1));
+console.log(tasksTypes([1, 2, 3, 4, 5], 2));
+console.log(tasksTypes([1, 2, 4, 2, 10, 3, 1, 4, 5, 4, 9, 8], 1));
