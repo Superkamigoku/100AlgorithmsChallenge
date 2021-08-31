33e2904dc96a703a0ebb90180c45b43a9e4f368b
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-31 11:34:41 +1000

    Completed

--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,7 +1,14 @@
 export function validTime(time: string): boolean {
+  let clock = time.split(':');
+  let hour = parseInt(clock[0]);
+  let minutes = parseInt(clock[1]);
 
+  if (hour > 24 || minutes > 60) {
+    return false;
+  }
+  return true;
 }
 
-// console.log(validTime('13:58'));
-// console.log(validTime('25:51'));
-// console.log(validTime('02:76'));
+console.log(validTime('13:58'));
+console.log(validTime('25:51'));
+console.log(validTime('02:76'));
