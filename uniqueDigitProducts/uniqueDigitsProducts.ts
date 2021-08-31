0e8e209b4c183e397e47475e691983ed4a627c66
Superkamigoku <Superkamigoku@hotmail.com>
2021-08-31 11:28:12 +1000

    Completed

--- uniqueDigitProducts/uniqueDigitsProducts.ts
+++ uniqueDigitProducts/uniqueDigitsProducts.ts
@@ -1,4 +1,20 @@
 export function uniqueDigitProducts(a: number[]): number {
+  let uniques = {};
+  let total = 0;
+  for (let i = 0; i < a.length; i++) {
+    const currentNumArr = a[i].toString().split('');
+    let multiple = 1;
+    for (let j = 0; j < currentNumArr.length; j++) {
+      multiple *= parseInt(currentNumArr[i]);
+    }
+
+    if (!uniques.hasOwnProperty(multiple)) {
+      uniques[multiple] = 1;
+      total++;
+    }
+  }
+
+  return total;
 }
 
-// console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
+console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
